a6d6692c8d6b7a8d734e508062bfbb9dd4c56038
Hardik Kanjariya <hkdevs@hardikkanajariya.in>
2025-09-03 09:59:30 +0530

    updated import paths

--- src/components/superduparadmin/ProtectedSuperDuparAdminRoute.tsx
+++ src/components/superduparadmin/ProtectedSuperDuparAdminRoute.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
-import { SuperDuparAdminAuthProvider, useSuperDuparAdminAuth } from '../../hooks/useSuperDuparAdminAuth2';
+import { SuperDuparAdminAuthProvider, useSuperDuparAdminAuth } from '../../hooks/useSuperDuparAdminAuth';
 
 interface ProtectedSuperDuparAdminRouteProps {
     children: React.ReactNode;
